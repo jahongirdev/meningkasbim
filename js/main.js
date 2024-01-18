277b10d9f556c606ed6f3938f80f4a62394326a7
Jahongir Murodboev <jahongirdev99@gmail.com>
2024-01-18 23:54:29 +0500

    adding responsive to hero

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-let openHam = document.querySelector('#openHam');
-let closeHam = document.querySelector('#closeHam');
-let navigationItems = document.querySelector('#site-menu');
+const openHam = document.querySelector('#openHam');
+const closeHam = document.querySelector('#closeHam');
+const navigationItems = document.querySelector('#site-menu');
+const navLink = document.querySelectorAll('#nav-link');
 
 const hamburgerEvent = (navigation, close, open) => {
     navigationItems.style.display = navigation;
@@ -10,3 +11,11 @@ const hamburgerEvent = (navigation, close, open) => {
 
 openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
 closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
+
+navLink.forEach(link => {
+    link.addEventListener('click', () => {
+        navigationItems.style.display = "none";
+        closeHam.style.display = "none";
+        openHam.style.display = "block";
+    });
+});
