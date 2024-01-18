2eddbef70f8040ec8a10edd04dd7597db207ef99
Jahongir Murodboev <jahongirdev99@gmail.com>
2024-01-18 22:17:30 +0500

    add header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-// Javascript codes here
+let openHam = document.querySelector('#openHam');
+let closeHam = document.querySelector('#closeHam');
+let navigationItems = document.querySelector('#site-menu');
+
+const hamburgerEvent = (navigation, close, open) => {
+    navigationItems.style.display = navigation;
+    closeHam.style.display = close;
+    openHam.style.display = open;
+};
+
+openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
+closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
